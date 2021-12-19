4a912d66b087da96c100889ef708b46abed4f169
Aleksandr <box-570@yandex.ru>
2021-12-19 19:22:40 +0300

    add commander

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Page loader utility.')
+  .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")');
+
+program.parse(process.argv);

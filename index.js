5fe32e66beb77a22e6b0e938c77196334de08434
Aleksandr <box-570@yandex.ru>
2022-01-12 22:59:25 +0300

    add mkdir function

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 import fs from 'fs/promises';
 import getCurrentPath from './src/currentPath.js';
-import pageLoading from './src/utils.js';
+import { pageLoading, mkdir } from './src/utils.js';
 
 export default (url, output) => {
   if (url) {
     const currentPath = getCurrentPath(url);
     pageLoading(url)
-      .then((page) => {
-        fs.mkdir(`${output}/__files`)
-          .then(() => {
-            fs.mkdir(`${output}/__files/${currentPath}`)
-              .then(() => {
-                fs.writeFile(`${output}/__files/${currentPath}/${currentPath}.html`, page);
-              });
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then((page) => mkdir(`${output}/__files`, page))
+      .then((page) => mkdir(`${output}/__files/${currentPath}`, page))
+      .then((page) => fs.writeFile(`${output}/__files/${currentPath}/${currentPath}.html`, page))
+      .catch((err) => console.log(err));
   }
 };
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,19 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
 import axios from 'axios';
 
-const pageLoading = (path) => {
-  axios.get(path)
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
-};
+export const pageLoading = (path) => axios.get(path)
+  .then((response) => response.data)
+  .catch((err) => console.log(err));
 
-export default pageLoading;
+export const mkdir = (path, data) => fs.access(path, constants.R_OK)
+  .then(() => Promise.resolve(data))
+  .catch(() => fs.mkdir(path, (err) => {
+    if (!err) {
+      return new Promise((res) => {
+        res(data);
+      });
+    }
+
+    return Promise.reject(new Error('error!'));
+  }));

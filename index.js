30e39cb84fc801d4876c08f68bebaafc0b0f1316
Aleksandr <box-570@yandex.ru>
2022-01-18 23:51:44 +0300

    fix getCurrentPath

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import { constants } from 'fs';
+import { parse } from 'path';
 import cheerio from 'cheerio';
 import getCurrentPath from './src/currentPath.js';
 import { fetchPage, fetchResourse } from './src/utils.js';
@@ -7,8 +8,7 @@ import { fetchPage, fetchResourse } from './src/utils.js';
 export default (url, output) => {
   if (url) {
     const currentPath = getCurrentPath(url);
-    // fetchPage(url)
-    fs.readFile(`./__fixtures__/ru-hexlet-io-courses.html`)
+    fetchPage(url)
       .then((page) => fs.writeFile(`${output}/${currentPath}.html`, page)
         .then(() => fs.access(`${output}/${currentPath}`, constants.R_OK)
           .then(() => fs.mkdir(`${output}/${currentPath}_files`).then(() => page))
@@ -18,16 +18,17 @@ export default (url, output) => {
       .then((page) => {
         const $ = cheerio.load(page);
         const images = $('img');
+        console.log(images.length)
 
         if (images.length) {
-          images.each((i, el) => {
-            const name = `${output}/${currentPath}_files/ru-hexlet-io-courses.jpg`;
-            console.log(name)
-            fetchResourse(el.attribs.src, name)
-              .then(() => {
-                console.log('done');
-              });
-          });
+          // images.each((i, el) => {
+          //   console.log(el.attribs.src)
+          //   const name = `${output}/${currentPath}_files/sad${i}.jpg`;
+          //   fetchResourse(el.attribs.src, name)
+          //     .then(() => {
+          //       console.log('done');
+          //     });
+          // });
         }
       })
       .catch((err) => console.log(err));
--- src/currentPath.js
+++ src/currentPath.js
@@ -1,8 +1,15 @@
-const getCurrentPath = (path) => {
-  const [, tt] = path.split('://');
+import { parse } from 'path';
 
-  return `${tt.split('.').join('-').split('/').join('-')
-    .split('?')[0]}`;
+const asdf = (part) => {
+  return part.split('.').join('-');
+}
+
+const getCurrentPath = (path) => {
+  const { dir, base } = parse(path);
+  const [, parts] = dir.split('://');
+  const result = parts.split('/').map((part) => asdf(part)).join('-');
+
+  return `${result}-${base}`;
 };
 
 export default getCurrentPath;

dd00b4fee941f4baed19c6a6eced36774e1e10e4
Aleksandr <box-570@yandex.ru>
2022-02-01 18:26:24 +0300

    add exit code 2

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { exit } from 'process';
 import main from '../index.js';
 
 const program = new Command();
 program
   .version('0.0.1', '-v, --version', 'output the version number')
   .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")', process.cwd())
-  .arguments('<url>')
+  .arguments('[url]')
   .action((url) => {
+    if (!url) {
+      exit(2);
+    }
     const { output } = program.opts();
     main(url, output);
   });

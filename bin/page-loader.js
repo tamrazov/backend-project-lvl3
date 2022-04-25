97910e2f4751367b53fc5941289e3fa2cc46310d
Alex <box-570@yandex.ru>
2022-04-25 21:42:44 +0300

    fix bugs

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import { readFileSync } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import loadingPage from '../src/index.js';
 
-const { version } = JSON.parse(readFileSync('./package.json'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const { version } = JSON.parse(readFileSync(path.resolve(__dirname, '../package.json')));
 const program = new Command();
 program
   .version(version, '-v, --version', 'output the version number')
@@ -12,7 +17,7 @@ program
   .action((url) => {
     const { output } = program.opts();
     loadingPage(url, output)
-      .then((path) => console.log(path))
+      .then((outPath) => console.log(outPath))
       .catch((err) => {
         console.error(err.message);
         process.exit(1);
